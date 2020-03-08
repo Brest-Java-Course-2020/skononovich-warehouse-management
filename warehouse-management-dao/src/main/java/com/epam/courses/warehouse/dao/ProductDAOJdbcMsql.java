fa43b7d7361703c282050e9b43d9470248a1d5f1
sergeykon <beergood6@gmail.com>
2020-03-09 00:54:58 +0300

    Add empty dao layer

---
+++ warehouse-management-dao/src/main/java/com/epam/courses/warehouse/dao/ProductDAOJdbcMsql.java
@@ -0,0 +1,33 @@
+package com.epam.courses.warehouse.dao;
+
+import com.epam.courses.warehouse.model.Product;
+
+import java.util.List;
+
+public class ProductDAOJdbcMsql implements ProductDAO {
+
+    @Override
+    public Integer create(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public Product getById(Integer productId) {
+        return null;
+    }
+
+    @Override
+    public Integer update(Product product) {
+        return null;
+    }
+
+    @Override
+    public Integer delete(Integer productId) {
+        return null;
+    }
+}
---
+++ warehouse-management-dao/src/main/java/com/epam/courses/warehouse/dao/ProductRecordDAOJdbcMsql.java
@@ -0,0 +1,30 @@
+package com.epam.courses.warehouse.dao;
+
+import com.epam.courses.warehouse.model.Product;
+import com.epam.courses.warehouse.model.ProductRecord;
+
+import java.sql.Date;
+import java.util.List;
+
+public class ProductRecordDAOJdbcMsql implements ProductRecordDAO {
+
+    @Override
+    public Integer create(ProductRecord productRecord) {
+        return null;
+    }
+
+    @Override
+    public List<ProductRecord> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<ProductRecord> getAllProductRecords(Date from, Date by) {
+        return null;
+    }
+
+    @Override
+    public Integer countProductInWarehouse(Product product) {
+        return null;
+    }
+}
