25580c63bcc3aebbe02351e8c7e374ad5ff7eb69
sergeykon <beergood6@gmail.com>
2020-03-23 15:10:22 +0300

    Refactor test ProductRecordDAOJdbcMsqlIT

--- warehouse-management-dao/src/test/java/com/epam/courses/warehouse/dao/ProductRecordDAOJdbcMsqlIT.java
+++ warehouse-management-dao/src/test/java/com/epam/courses/warehouse/dao/ProductRecordDAOJdbcMsqlIT.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -26,16 +25,12 @@ public class ProductRecordDAOJdbcMsqlIT {
     private static final Date RECORD_DATE = Date.valueOf("2020-05-30");
     private static final DealTypes DEAL_TYPE = DealTypes.DELIVERY;
 
-    private static final int PRODUCT_COUNT = 7;
-
-    private static final int PRODUCT_RECORDS_IN_DATABASE = 5;
-
-    private static final Date FROM_DATE = Date.valueOf("2020-01-12");
-    private static final Date TO_DATE = Date.valueOf("2020-02-01");
-    private static final int RECORDS_IN_TIME_INTERVAL = 3;
-
     @Autowired
     private ProductRecordDAOJdbcMsql productRecordDAO;
+
+    //TODO: This is good practice?
+    @Autowired
+    private ProductRecordDtoDAOJdbcMsql productRecordDtoDAO;
 
     @Test
     public void shouldCreateProductRecord(){
@@ -45,32 +40,11 @@ public class ProductRecordDAOJdbcMsqlIT {
                 .setProductRecordDate(RECORD_DATE)
                 .setDealType(DEAL_TYPE);
 
-        int sizeBeforeCreateNewRecord = productRecordDAO.getAll().size();
+        int sizeBeforeCreateNewRecord = productRecordDtoDAO.getAll().size();
         Integer addedProductRecordId = productRecordDAO.create(productRecord);
-        int sizeAfterCreateNewRecord = productRecordDAO.getAll().size();
+        int sizeAfterCreateNewRecord = productRecordDtoDAO.getAll().size();
 
         assertNotNull(addedProductRecordId);
         assertEquals(sizeBeforeCreateNewRecord + 1, sizeAfterCreateNewRecord);
-    }
-
-    @Test
-    public void shouldGetAllProductRecords(){
-        List<ProductRecord> productRecordList = productRecordDAO.getAll();
-        assertNotNull(productRecordList);
-        assertEquals(PRODUCT_RECORDS_IN_DATABASE, productRecordList.size());
-    }
-
-    @Test
-    public void shouldGetAllProductRecordsInTimeInterval(){
-        List<ProductRecord> productRecordListInTimeInterval = productRecordDAO.getAllProductRecords(FROM_DATE, TO_DATE);
-        assertNotNull(productRecordListInTimeInterval);
-        assertEquals(RECORDS_IN_TIME_INTERVAL, productRecordListInTimeInterval.size());
-    }
-
-    @Test
-    public void shouldGetCountProductInWarehouse(){
-        int countOfProduct = productRecordDAO.countProductInWarehouse(PRODUCT_ID);
-
-        assertEquals(PRODUCT_COUNT, countOfProduct);
     }
 }
