467aed16036c23a573bc6e00dfe216122f4b00e4
sergeykon <beergood6@gmail.com>
2020-03-23 15:29:14 +0300

    Add ProductRecordDtoDAOJdbcMsql implementation

---
+++ warehouse-management-dao/src/main/java/com/epam/courses/warehouse/dao/ProductRecordDtoDAOJdbcMsql.java
@@ -0,0 +1,49 @@
+package com.epam.courses.warehouse.dao;
+
+import com.epam.courses.warehouse.dao.rowMapper.ProductRecordDtoRowMapper;
+import com.epam.courses.warehouse.model.dto.ProductRecordDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.List;
+
+@Repository
+public class ProductRecordDtoDAOJdbcMsql implements ProductRecordDtoDAO {
+
+    private Logger LOGGER = LoggerFactory.getLogger(ProductRecordDtoDAOJdbcMsql.class);
+
+    @Value("${recordDto.getAll}")
+    private String sqlGetAll;
+
+    @Value("${recordDto.getAllInTimeInterval}")
+    private String sqlGetAllInTimeInterval;
+
+    private NamedParameterJdbcTemplate jdbcTemplate;
+    private ProductRecordDtoRowMapper productRecordDtoRowMapper;
+
+    public ProductRecordDtoDAOJdbcMsql(NamedParameterJdbcTemplate namedParameterJdbcTemplate,
+                                       ProductRecordDtoRowMapper productRecordDtoRowMapper){
+        this.jdbcTemplate = namedParameterJdbcTemplate;
+        this.productRecordDtoRowMapper = productRecordDtoRowMapper;
+    }
+
+    @Override
+    public List<ProductRecordDTO> getAll() {
+        LOGGER.debug("ProductRecordDAOJdbcMsql:getAll");
+        return jdbcTemplate.query(sqlGetAll, productRecordDtoRowMapper);
+    }
+
+    @Override
+    public List<ProductRecordDTO> getAllInTimeInterval(Date from, Date to) {
+        LOGGER .debug("ProductRecordDAOJdbcMsql:getAllInTimeInterval from: " + from.toString() + " to: " + to);
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource()
+                .addValue("from", from)
+                .addValue("to", to);
+        return jdbcTemplate.query(sqlGetAllInTimeInterval, parameterSource, productRecordDtoRowMapper);
+    }
+}
---
+++ warehouse-management-dao/src/main/java/com/epam/courses/warehouse/dao/rowMapper/ProductRecordDtoRowMapper.java
@@ -0,0 +1,20 @@
+package com.epam.courses.warehouse.dao.rowMapper;
+
+import com.epam.courses.warehouse.model.DealTypes;
+import com.epam.courses.warehouse.model.dto.ProductRecordDTO;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductRecordDtoRowMapper implements RowMapper<ProductRecordDTO> {
+    @Override
+    public ProductRecordDTO mapRow(ResultSet resultSet, int i) throws SQLException {
+        ProductRecordDTO productRecordDTO = new ProductRecordDTO()
+                .setProductName(resultSet.getString("product_name"))
+                .setQuantityOfProduct(resultSet.getInt("quantity"))
+                .setDealDate(resultSet.getDate("deal_date"))
+                .setDealType(DealTypes.fromInt(resultSet.getInt("deal_type")));
+        return productRecordDTO;
+    }
+}
