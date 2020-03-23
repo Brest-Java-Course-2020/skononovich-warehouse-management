93d0ef5581ec77db455ddfa93fe3f6609d2d5043
sergeykon <beergood6@gmail.com>
2020-03-23 14:49:02 +0300

    Add deal types enum

--- warehouse-management-model/src/main/java/com/epam/courses/warehouse/model/DealTypes.java
+++ warehouse-management-model/src/main/java/com/epam/courses/warehouse/model/DealTypes.java
@@ -4,5 +4,24 @@ package com.epam.courses.warehouse.model;
  * Enum deal types.
  */
 public enum DealTypes {
-    GETTING, DELIVERY
+    GETTING(0), DELIVERY(1);
+    private int value;
+
+    private DealTypes(int value){
+        this.value = value;
+    }
+
+    public static DealTypes fromInt(int x) {
+        switch(x) {
+            case 0:
+                return GETTING;
+            case 1:
+                return DELIVERY;
+        }
+        return null;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
