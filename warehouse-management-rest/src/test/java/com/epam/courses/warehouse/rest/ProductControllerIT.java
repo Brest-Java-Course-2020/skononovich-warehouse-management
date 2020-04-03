76236aad237e4d9800e2dbc26af2b1a3f6cb6f79
sergeykon <beergood6@gmail.com>
2020-04-03 07:08:49 +0300

    Rewrite integration test to ProductController

--- warehouse-management-rest/src/test/java/com/epam/courses/warehouse/rest/ProductControllerIT.java
+++ warehouse-management-rest/src/test/java/com/epam/courses/warehouse/rest/ProductControllerIT.java
@@ -1,14 +1,23 @@
 package com.epam.courses.warehouse.rest;
 
 import com.epam.courses.warehouse.model.Product;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -17,78 +26,139 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath:app-context-test.xml"})
-@Transactional
 @Rollback
+@Transactional
 public class ProductControllerIT {
 
     public static final String TEST_PRODUCT_NAME = "testProduct";
+    public static final String PRODUCTS_ENDPOINT = "/products";
 
     @Autowired
     private ProductController productController;
 
-    @Test
-    public void shouldGetAllProducts(){
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
-        List<Product> productList = (List<Product>) productController.getAll();
-
-        assertNotNull(productList);
-        assertTrue(productList.size() > 0);
+    @BeforeEach
+    public void before(){
+        mockMvc = MockMvcBuilders.standaloneSetup(productController)
+                .setMessageConverters(new MappingJackson2HttpMessageConverter())
+                .build();
     }
 
     @Test
-    public void shouldCreateAndGetProductById(){
-        Integer productId = shouldCreateProduct(TEST_PRODUCT_NAME);
-        Product product = shouldGetProductById(productId);
+    public void shouldGetAllProducts() throws Exception {
+        getAllProducts();
+    }
+
+    @Test
+    public void shouldCreateAndGetProductById() throws Exception {
+
+        Integer productId = createProduct(TEST_PRODUCT_NAME);
+        Product product = getProductById(productId);
 
         assertEquals(TEST_PRODUCT_NAME, product.getProductName());
     }
 
-    private Integer shouldCreateProduct(String productName){
-        ResponseEntity<Integer> response = productController.create(productName);
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertNotNull(response.getBody());
-
-        return response.getBody();
-    }
-
-    private Product shouldGetProductById(Integer productId){
-        ResponseEntity<Product> responseProduct = productController.findById(productId);
-        assertNotNull(responseProduct);
-        assertEquals(HttpStatus.OK, responseProduct.getStatusCode());
-        assertNotNull(responseProduct.getBody());
-
-        return responseProduct.getBody();
-    }
-
     @Test
-    public void shouldUpdateProduct(){
-        Integer productId = shouldCreateProduct(TEST_PRODUCT_NAME);
+    public void shouldUpdateProduct() throws Exception {
+
+        Integer productId = createProduct(TEST_PRODUCT_NAME);
 
         Product product = new Product()
                 .setProductId(productId)
                 .setProductName("newName");
 
-        Integer numberOfUpdatedRecords = productController.update(product).getBody();
-        assertEquals((Integer)1, numberOfUpdatedRecords);
+        MockHttpServletResponse response = mockMvc.perform(
+                MockMvcRequestBuilders.put(PRODUCTS_ENDPOINT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(product))
+        ).andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
 
-        Product updatedProduct = shouldGetProductById(productId);
+        assertNotNull(response);
 
+        int numberOfUpdatedRecords = objectMapper.readValue(response.getContentAsString(), Integer.class);
+
+        assertEquals(1, numberOfUpdatedRecords);
+
+        Product updatedProduct = getProductById(productId);
+
+        assertNotNull(updatedProduct);
         assertEquals(product.getProductName(), updatedProduct.getProductName());
     }
 
     @Test
-    public void shouldDeleteProduct(){
-        Integer productId = shouldCreateProduct(TEST_PRODUCT_NAME);
+    public void shouldDeleteProduct() throws Exception {
+        Integer productId = createProduct(TEST_PRODUCT_NAME);
 
-        int sizeBeforeDelete = productController.getAll().size();
+        int sizeBeforeDelete = getAllProducts().size();
 
-        Integer numberOfDeletedRecords = productController.delete(productId).getBody();
-        assertEquals((Integer)1, numberOfDeletedRecords);
+        MockHttpServletResponse response = mockMvc.perform(
+                MockMvcRequestBuilders.delete(PRODUCTS_ENDPOINT + "/" + productId)
+        ).andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
 
-        int sizeAfterDelete = productController.getAll().size();
+        int numberOfDeletedRecords = objectMapper.readValue(response.getContentAsString(), Integer.class);
+        assertEquals(1, numberOfDeletedRecords);
+
+        int sizeAfterDelete = getAllProducts().size();
 
         assertEquals(sizeBeforeDelete, sizeAfterDelete + 1);
+    }
+
+    private List<Product> getAllProducts() throws Exception {
+
+        MockHttpServletResponse response = mockMvc.perform(
+                MockMvcRequestBuilders.get(PRODUCTS_ENDPOINT)
+        ).andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        List<Product> productList = objectMapper.readValue(response.getContentAsString(),
+                new TypeReference<List<Product>>(){});
+
+        assertNotNull(productList);
+        assertTrue(productList.size() > 0);
+
+        return productList;
+    }
+
+    private Integer createProduct(String productName) throws Exception {
+
+        MockHttpServletResponse response = mockMvc.perform(
+                MockMvcRequestBuilders.post(PRODUCTS_ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(productName))
+        ).andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        Integer productId = objectMapper.readValue(response.getContentAsString(), Integer.class);
+        assertNotNull(productId);
+
+        return productId;
+    }
+
+    private Product getProductById(Integer productId) throws Exception {
+
+        MockHttpServletResponse response = mockMvc.perform(
+                MockMvcRequestBuilders.get(PRODUCTS_ENDPOINT + "/" + productId)
+        ).andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        Product responseProduct = objectMapper.readValue(response.getContentAsString(), Product.class);
+
+        assertNotNull(responseProduct);
+
+        return responseProduct;
     }
 }
