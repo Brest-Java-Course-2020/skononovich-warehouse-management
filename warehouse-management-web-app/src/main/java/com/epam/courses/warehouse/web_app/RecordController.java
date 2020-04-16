668eecf8cb2bf677206e178692c1b8c1678c6196
sergeykon <beergood6@gmail.com>
2020-04-16 04:40:54 +0300

    Add date filter to html

--- warehouse-management-web-app/src/main/java/com/epam/courses/warehouse/web_app/RecordController.java
+++ warehouse-management-web-app/src/main/java/com/epam/courses/warehouse/web_app/RecordController.java
@@ -1,5 +1,6 @@
 package com.epam.courses.warehouse.web_app;
 
+import com.epam.courses.warehouse.model.filter.ProductRecordDateInterval;
 import com.epam.courses.warehouse.service_rest.ProductRecordDtoServiceRest;
 import com.epam.courses.warehouse.service_rest.ProductRecordServiceRest;
 import org.slf4j.Logger;
@@ -7,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class RecordController {
@@ -26,8 +28,23 @@ public class RecordController {
     public final String records(Model model){
         LOGGER.debug("RecordController:records");
 
+        ProductRecordDateInterval interval = new ProductRecordDateInterval();
+
+        model.addAttribute("interval", interval);
         model.addAttribute("records", productRecordDtoServiceRest.getAll());
         return "circulation";
     }
+
+    @GetMapping("/filter")
+    public final String filterRecords(@ModelAttribute("interval") ProductRecordDateInterval interval,
+                               Model model) {
+        model.addAttribute("interval", interval);
+        model.addAttribute("records", productRecordDtoServiceRest.getAllInTimeInterval(
+                interval.getStartInterval(),
+                interval.getEndInterval()));
+        return "circulation";
+    }
+
+
 
 }
