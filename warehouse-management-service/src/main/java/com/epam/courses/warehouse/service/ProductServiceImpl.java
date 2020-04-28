0d8bd710447e9848db597a3267bec6fac1590cfd
sergeykon <beergood6@gmail.com>
2020-04-28 05:48:44 +0300

    maven-pmd-plugin

--- warehouse-management-service/src/main/java/com/epam/courses/warehouse/service/ProductServiceImpl.java
+++ warehouse-management-service/src/main/java/com/epam/courses/warehouse/service/ProductServiceImpl.java
@@ -4,7 +4,6 @@ import com.epam.courses.warehouse.dao.ProductDAO;
 import com.epam.courses.warehouse.model.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
